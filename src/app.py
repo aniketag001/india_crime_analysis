15691bca86bec1b5228188369b17489e9784874b
sakshitechworld <sakshi22jaiswal@gmail.com>
2024-12-19 18:02:43 +0530

    Added tabs

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
 from static_visualization import static_insights
+from dynamic_visualization import dynamic_insights
 
-static_insights()
+import streamlit as st
 
+kpis, dashboard = st.tabs(["KPIs", "Dashboard"])
+
+with kpis:
+    static_insights()
+with dashboard
+    dynamic_insights()
