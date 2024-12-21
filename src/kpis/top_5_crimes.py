a27df74fde1ee9721b22a53241134d9e73c818c9
Aniket Agarkar <aniket.agarkar.ds@ghrietn.raisoni.net>
2024-12-21 18:44:12 +0530

    Update top_5_crimes.py

--- src/kpis/top_5_crimes.py
+++ src/kpis/top_5_crimes.py
@@ -16,8 +16,8 @@ def top_5_crimes(df, population_df, literacy_df):
         """
     )
   crime_type_cols = ['Murder', 'Assault on women', 'Kidnapping and Abduction', 'Dacoity', 'Robbery',
-                  'Arson', 'Hurt', 'Prevention of atrocities (POA) Act', 'Protection of Civil Rights (PCR) Act',
-                  'Other Crimes Against SCs']
+                     'Arson', 'Hurt', 'Prevention of atrocities (POA) Act', 'Protection of Civil Rights (PCR) Act',
+                     'Other Crimes Against SCs']
   crimes_type = df[crime_type_cols].sum()
   top_crimes = crimes_type.nlargest(5)
   fig = plt.figure(figsize=(10, 8))
