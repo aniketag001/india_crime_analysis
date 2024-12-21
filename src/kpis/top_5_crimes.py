e7228cb73e72cce147025604a87e4510cdb6e3d6
Aniket Agarkar <aniket.agarkar.ds@ghrietn.raisoni.net>
2024-12-21 18:52:45 +0530

    Update top_5_crimes.py

--- src/kpis/top_5_crimes.py
+++ src/kpis/top_5_crimes.py
@@ -15,13 +15,13 @@ def top_5_crimes(df, population_df, literacy_df):
        with 44.7% of the top 5 crimes.
         """
     )
-  crime_type_cols = ['Murder', 'Assault on women', 'Kidnapping and Abduction', 'Dacoity', 'Robbery',
-                     'Arson', 'Hurt', 'Prevention of atrocities (POA) Act', 'Protection of Civil Rights (PCR) Act',
-                     'Other Crimes Against SCs']
-  crimes_type = df[crime_type_cols].sum()
-  top_crimes = crimes_type.nlargest(5)
-  fig = plt.figure(figsize=(10, 8))
-  plt.pie(top_crimes, labels=top_crimes.index, autopct='%1.1f%%', startangle=140)
-  plt.title("Top 5 Crime Types with Highest Occurrences")
-  plt.tight_layout()
-  st.pyplot(fig)
+    crime_type_cols = ['Murder', 'Assault on women', 'Kidnapping and Abduction', 'Dacoity', 'Robbery',
+                       'Arson', 'Hurt', 'Prevention of atrocities (POA) Act', 'Protection of Civil Rights (PCR) Act',
+                       'Other Crimes Against SCs']
+    crimes_type = df[crime_type_cols].sum()
+    top_crimes = crimes_type.nlargest(5)
+    fig = plt.figure(figsize=(10, 8))
+    plt.pie(top_crimes, labels=top_crimes.index, autopct='%1.1f%%', startangle=140)
+    plt.title("Top 5 Crime Types with Highest Occurrences")
+    plt.tight_layout()
+    st.pyplot(fig)
