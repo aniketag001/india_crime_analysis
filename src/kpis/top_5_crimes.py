8ab1e6a4fb05104837968d3420b62b1f949a856b
Aniket Agarkar <aniket.agarkar.ds@ghrietn.raisoni.net>
2024-12-21 17:45:46 +0530

    Update top_5_crimes.py

---
+++ src/kpis/top_5_crimes.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+
+def top_5_crimes(df,)
+crime_type_cols = ['Murder', 'Assault on women', 'Kidnapping and Abduction', 'Dacoity', 'Robbery',
+                  'Arson', 'Hurt', 'Prevention of atrocities (POA) Act', 'Protection of Civil Rights (PCR) Act',
+                  'Other Crimes Against SCs']
+crimes_type = df[crime_type_cols].sum()
+top_crimes = crimes_type.nlargest(5)
+fig = plt.figure(figsize=(10, 8))
+plt.pie(top_crimes, labels=top_crimes.index, autopct='%1.1f%%', startangle=140)
+plt.title("Top 5 Crime Types with Highest Occurrences")
+plt.tight_layout()
+st.pyplot(fig)
