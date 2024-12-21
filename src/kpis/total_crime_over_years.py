c9f7c64abf5d092b280df63b51da5531c01f4596
SayantiSaha014 <sayantisaha014@gmail.com>
2024-12-21 17:18:01 +0530

    Update total_crime_over_years.py

--- src/kpis/total_crime_over_years.py
+++ src/kpis/total_crime_over_years.py
@@ -9,7 +9,14 @@ def total_crime_over_years(df, population_df, literacy_df):
     
     st.subheader("Total Crime Over Year")
     st.text("Author: Sayanti Saha")
-
+    st.write(
+        """
+        The graph indicates notable fluctuations in crime rates over the years, with peaks in 2002 and 2012. 
+        The number of crimes was highest in 2002 and 2012, with around 33,500 crimes.
+        A sharp decline in crime rates is observed from 2002 to 2003, suggesting effective crime prevention measures or reporting changes.
+        Following the initial drop, there is a steady rise in crime rates from 2003 to 2008, indicating potential challenges in law enforcement or emerging crime trends.
+        """
+    )
     yearly_crime = df.groupby('Year')['Total crimes'].sum().reset_index()
     yearly_crime
 
